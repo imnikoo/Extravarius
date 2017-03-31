39afaba679855bff4ccee7faa08a3223e72c8b6d
Nick Efruev <myef@isd.dp.ua>
2017-03-31 20:14:35 +0300

    Special block ended

--- dev/js/containers/FrontPart/ContentBlock/contentBlock.js
+++ dev/js/containers/FrontPart/ContentBlock/contentBlock.js
@@ -6,8 +6,18 @@ class ContentBlock extends Component {
         return (
             <div className="content-block">
                 <div className="content-unit special">
-                    <div className="content-unit__diagonal">
+                    <div className="shifted-info">
+                        <div className="shifted-info__date">
+                            <span>12</span><span>\09\2016</span>
+                        </div>
+                        <span className="shifted-info__action-title">Лекція-експеримент</span>
+                        <span className="shifted-info__action">ДИФУЗІЯ. ВЗАЄМОДІЯ МОЛЕКУЛ ТА АТОМІВ У ПРИРОДІ.</span>
+                        <div className="buttons">
+                            <div className="buttons__details">ДЕТАЛЬНІШЕ</div>
+                            <div className="buttons__checkout-tickets">ПРИДБАТИ КВИТКИ</div>
+                        </div>
                     </div>
+                    <div className="content-unit__diagonal" />
                 </div>
                 <div className="content-unit purple">
                 </div>
